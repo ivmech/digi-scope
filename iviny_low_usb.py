2e59e174cda82d182daa0293004bcd69be46d29d
ivmech <bilgi@ivmech.com>
2016-09-21 22:44:17 +0300

    initial commit

---
+++ iviny_low_usb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+
+PATH = os.getcwd()
+LIBPATH = os.path.join(PATH, "lib")
+sys.path.append(LIBPATH)
+from usbdevice import IVinyUSBDevice
+
+while True:
+    try:
+        iviny = IVinyUSBDevice(idVendor=0x16c0, idProduct=0x05df)
+        break
+    except:
+        time.sleep(1)
+
+time_list = []
+data_list = []
+start_time = time.time()
+elapsed_time = 0
+
+while elapsed_time < 2:
+    if iviny:
+        iviny.write(ord("1"))
+        raw_data=iviny.read()
+        #print raw_data
+
+        data = "".join([chr(x) for x in raw_data if x != 255])
+        #print data
+        elapsed_time = time.time() - start_time
+        time_list.append(elapsed_time)
+        analogs = data.split(";")[0]
+        analog1 = analogs.split(",")[0]
+        data_list.append(analog1)
+
+    time.sleep(0.01)
+
+savefile = open(os.path.join(PATH, "test.ivs"), "w")
+write = list(zip(time_list, data_list))
+savefile.writelines(["%.7f,%s\n" % item for item in write])
+savefile.close()
